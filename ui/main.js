e9b6fb9ca2d85102056b46c35da3e8c2f87a0963
MayTune <may05tune@gmail.com>
2016-09-29 16:41:16 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,14 +1,8 @@
-console.log('Loaded!');
-var hi=document.getElementById("maintext");
-hi.innerHTML="Hi there!";
-var pic=document.getElementById("madi");
-var marginLeft=0;
-function moveRight()
-{
-    marginLeft=marginLeft+10;
-    pic.style.marginLeft=marginLeft+'px';
-}
-pic.onclick=function(){
-    var interval=setInterval(moveRight,10);
-    pic.style.marginLeft="100px";
-}
+var button=document.getElementById("click");
+var counter =0;
+button.onclick=function(){
+    
+    counter =counter+1;
+    var span=document.getElementById("count");
+    span.innerHTML=counter.toString();
+};
