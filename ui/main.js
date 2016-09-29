bce1386ae78105503566ea9dfd817f5fd621c40a
MayTune <may05tune@gmail.com>
2016-09-29 15:43:14 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -5,10 +5,10 @@ var pic=document.getElementById("madi");
 var marginLeft=0;
 function moveRight()
 {
-    marginLeft=marginLeft+10;
+    marginLeft=marginLeft+1;
     pic.style.marginLeft=marginLeft+'px';
 }
 pic.onclick=function(){
-    var interval=setInterval(moveRight,100);
+    var interval=setInterval(moveRight,10);
     pic.style.marginLeft="100px";
 }
