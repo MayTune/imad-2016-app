0c24c5da2fcb0bb926addea697ba4a5a2e86eb22
MayTune <may05tune@gmail.com>
2016-10-27 23:19:35 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -3,11 +3,10 @@ button.onclick=function(){
     
     var request=new XMLHttpRequest();
     request.onreadystatechange=function(){
-        if(request.readystate===XMLHttpRequest.DONE){
+        if(request.readyState===XMLHttpRequest.DONE){
             if(request.status===200)
             {
                var counter=request.responseText; 
-               alert(counter);
                var span=document.getElementById("count");
                span.innerHTML=counter;
             }
