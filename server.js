2de8ddce5c9c8d8e83d8dee774c7a825ee4f18c1
Aishwarya Nair <106115005@nitt.edu>
2016-11-30 14:02:06 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -99,7 +99,7 @@ function createTemplate(data){
             <hr>
             <h3> ${heading}</h3>
             <div>
-                ${date}
+                ${date.toDateString()}
             </div>
             <div>
                 ${content}
