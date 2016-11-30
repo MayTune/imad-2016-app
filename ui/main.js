e156db0aa9dfd726db886963292e38404ed9325a
Aishwarya Nair <106115005@nitt.edu>
2016-11-30 20:33:47 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,3 +1,36 @@
+var submit=document.getElementById('submit_btn');
+submit.onclick=function(){
+    var request=new XMLHttpRequest();
+    request.onreadystatechange=function(){
+        if(request.readyState===XMLHttpRequest.DONE){
+            if(request.status===200)
+            {
+               console.log('User logged in');
+               alert('Logged in successfully');
+            }
+            else if(request.status===403)
+            {
+                alert('Username/Password incorrect');
+            }
+            else if(request.status===500)
+            {
+                alert('Something went wrong on the server');
+            }
+        }
+    };
+    var username=document.getElementById('username').value;
+    var password=document.getElementById('password').value;
+    console.log(username);
+    console.log(password);
+    request.open('POST','http://maytune.imad.hasura-app.io/login',true);
+    request.setRequestHeader('Content-Type','application/json');
+    request.send(JSON.stringify({username:username,password:password}));
+    
+};
+
+
+
+
 var button=document.getElementById("click");
 button.onclick=function(){
     
