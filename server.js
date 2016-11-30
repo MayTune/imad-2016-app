a48d5467dd9029eda596b1ba2b04f1056b1f4707
Aishwarya Nair <106115005@nitt.edu>
2016-11-30 21:28:05 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -124,7 +124,10 @@ app.get('/check-login',function(req,res){
        res.send('You are not logged in');
    }
 });
-
+app.get('/logout',function(req,res){
+   delete(req.session.outh);
+   res.send('Logged out');
+});
 
 var pool = new Pool(config);
 app.get('/test-db',function(req,res){
