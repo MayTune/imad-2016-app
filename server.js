aa27fd2caa6e9205cd2eaace9cdf607ca887d292
Aishwarya Nair <106115005@nitt.edu>
2016-11-30 20:33:47 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -60,7 +60,7 @@ function createTemplate(data){
 }
 function hash(input,salt)
 {
-    var hashed=crypto.pbkdf2Sync(input,salt,10000,512, 'sha512');
+    var hashed=crypto.pbkdf2Sync(input,salt,10000,512,'sha512');
     return["pbkdf2","10000",salt,hashed.toString('hex')].join('$');
 }
 app.get('/hash/:input',function(req,res){
