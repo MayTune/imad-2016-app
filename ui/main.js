5a8f3a42b335854cfcb2c88a03a8cad734595427
Aishwarya Nair <106115005@nitt.edu>
2016-11-30 22:26:59 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -120,6 +120,7 @@ function loadLogin () {
     request.onreadystatechange = function () {
         if (request.readyState === XMLHttpRequest.DONE) {
             if (request.status === 200) {
+                alert('hi');
                 loadLoggedInUser(this.responseText);
             } else {
                 loadLoginForm();
